9bd48ffd4cbf036f802a1c418f52d137bf370bab
Djany Mouton <djany.developpement@gmail.com>
2022-09-14 21:33:04 +0200

    Premier commit du projet All in one

---
+++ script.js
@@ -0,0 +1,44 @@
+    // MODE SOMBRE ET CLAIR //
+
+let button = document.querySelector('#mode');
+let span   = document.querySelector('span');
+
+if(localStorage.getItem('theme')){
+  if(localStorage.getItem('theme') == 'sombre') {
+    modeSombre();
+  }
+}
+
+button.addEventListener('click', () => {
+  if(document.body.classList.contains('dark')) {
+    document.body.className = '';
+    span.textContent = 'DARK';
+    localStorage.setItem('theme', 'clair');
+  }
+  else {
+    modeSombre();
+  }
+});
+
+function modeSombre() {
+  document.body.className = 'dark';
+  span.textContent = 'LIGHT';
+  localStorage.setItem('theme', 'sombre');
+}
+
+// API Bitcoin and altcoins price //
+
+const url = 'https://blockchain.info/ticker';
+
+async function recupererPrix() {
+  const requete = await fetch(url, {
+    method: 'GET'
+  });
+  
+  if(!requete.ok) {
+    alert('Un problème est survenu.');
+  } else {
+    let donnees = await requete.json(); document.querySelector('#price_label').textContent = donnees.EUR.last;
+  }
+}
+setInterval(recupererPrix, 1000);
